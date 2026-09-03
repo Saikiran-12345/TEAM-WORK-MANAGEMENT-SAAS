d36ef89cf6dbfce782407acd2e1c0159660fe0b1
Admin User <admin@example.com>
2026-09-03 23:30:25 +0530

    Added string helpers

---
+++ src/utils/stringUtils.ts
@@ -0,0 +1,2 @@
+﻿export const capitalize = (s: string) => s.charAt(0).toUpperCase() + s.slice(1);
+export const truncate = (s: string, len: number) => s.length > len ? s.substring(0, len) + '...' : s;
